be8644d4b664de03c36de1057b75bf68ae9aa6dc
qazal <qazal.software@gmail.com>
2025-01-12 14:44:31 +0200

    view

--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -490,9 +490,14 @@ def unbind_variable(ctx:ScheduleContext, bind:UOp, var:UOp, val:UOp):
   ctx.var_vals[ret:=var.replace(src=())] = val.src[1].const_arg
   return ret.valid(unwrap(bind.st))
 
+def mv_const(st:UOp, x:UOp):
+  flat_const = UOp.const(x.dtype.base, x.const_arg)
+  if all(v.mask is None for v in unwrap(st.st).views): return flat_const
+  return flat_const.valid(unwrap(st.st))
+
 break_sched = PatternMatcher([
-  # VIEW(CONST) becomes VALID (TODO: this should only be done for masked views)
-  (UPat(Ops.VIEW, name="st", src=(UPat.cvar("x"),)), lambda st,x: UOp.const(x.dtype.base, x.const_arg).valid(st.st)),
+  # masked VIEW(CONST) becomes VALID
+  (UPat(Ops.VIEW, name="st", src=(UPat.cvar("x"),)), mv_const),
   (UPat(Ops.BIND, name="bind", src=(UPat.var("var"), UPat.var("val"))), unbind_variable),
   # bufferized uops either becomes a VIEW(BUFFER) or we VIEW the uop and delete the BUFFER
   (UPat(Ops.VIEW, name="st", src=(UPat(Ops.BUFFER, name="buffer"), UPat.var("x"))), store_or_fuse),
@@ -539,7 +544,7 @@ def create_schedule_with_vars(outs:list[UOp], skip_check:bool=not __debug__) -> 
   graph_rewrite(sink, do_realize+create_ctx, ctx)
   # group realizes into kernels
   store_groups = group_realizes(ctx)
-  graph_rewrite(sink, break_sched, ctx)
+  graph_rewrite(sink, remove_movement_ops+break_sched, ctx)
   # preschedule realize groups
   prescheduled: list[ScheduleItem] = []
   for store_uops in store_groups:
